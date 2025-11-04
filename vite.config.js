2f49ebda7912a07e0567847ed9058e09ad9aa4a6
namaah00 <kem.naam@gmail.com>
2025-11-04 21:47:48 +0100

    t8

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,12 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: './', // Ważne dla GitHub Pages!
   server: {
     port: 5173,
     host: true
+  },
+  build: {
+    outDir: 'dist'
   }
 });
