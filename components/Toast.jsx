a9ebe52f2eff5ccc33e0dae21f62b09ece3737d2
namaah00 <kem.naam@gmail.com>
2025-11-05 18:22:57 +0100

    komenatrze toast

--- components/Toast.jsx
+++ components/Toast.jsx
@@ -1,4 +1,9 @@
+//utworzenie funkcji, która ma domyślnie typ powiadomienia sukces
 export default function Toast({ message, type = 'success' }) {
+ 
+  //utworzenie kontenera powiadomienia
+  //do powiadomien pokazane są ikonki dla wizualnego efektu
+  //przykład: <Toast message="Dane zapisane pomyślnie!" type="success" />
   return (
     <div className={`toast toast-${type}`}>
       {type === 'success' && '✅ '}
