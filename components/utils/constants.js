dbfb8622418dd2e6435c90eb60635078339c2651
namaah00 <kem.naam@gmail.com>
2026-01-19 20:22:19 +0000

    cons com

--- components/utils/constants.js
+++ components/utils/constants.js
@@ -1,26 +1,26 @@
-// Klucze localStorage
+//klucze localStorage (nazwy pod którymi dane są przechowywane w przeglądarce)
 export const STORAGE_KEYS = {
-  COMMENTS: 'matrix-comments',
-  SOURCES: 'pe004-sources',
-  LANGUAGE: 'app-language',
-  THEME: 'app-theme'
+  COMMENTS: 'matrix-comments', //wszystkie komentarze matrycy
+  SOURCES: 'pe004-sources', //źródła dynamiczne 004
+  LANGUAGE: 'app-language', //aktualny język
+  THEME: 'app-theme' //aktualny motyw
 };
 
-// Domyślne wartości
+//domyślne wartości (używane przy inicjacji hooków localStorage)
 export const DEFAULT_VALUES = {
-  LANGUAGE: 'pl',
-  THEME: 'light',
-  COMMENTS: {},
-  SOURCES: []
+  LANGUAGE: 'pl', //domyślny język
+  THEME: 'light', //domyślny motyw
+  COMMENTS: {}, //początkowo brak komentarzy
+  SOURCES: [] //początkowo brak źródeł
 };
 
-// Wersja formatu eksportu
+//wersja formatu eksportu json (dla kompatybilności)
 export const EXPORT_VERSION = '2.0';
 
-// Czas wyświetlania toastu (ms)
+//czas wyświetlania powiadomień (ms) = 3s
 export const TOAST_DURATION = 3000;
 
-// Limity
+//limity wielkości dla obrazów w komentarzach
 export const LIMITS = {
   IMAGE_SIZE_MB: 2,
   IMAGE_SIZE_BYTES: 2 * 1024 * 1024
