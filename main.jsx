a0522cd84b3e5dcb868642688d0c6117609cbbde
namaah00 <kem.naam@gmail.com>
2026-01-19 04:47:30 +0000

    kom main

--- main.jsx
+++ main.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom/client'; //DOM przekształca kod HTML w strukturę drzewa obiektów
 import App from './App.jsx';
 import './styles/simple.css';
 
+//utworzenie kontener reacta w root index.html
 ReactDOM.createRoot(document.getElementById('root')).render(
+  //włożenie komponentu <App /> do #root i uruchomienie aplikacji
   <React.StrictMode>
     <App />
   </React.StrictMode>
