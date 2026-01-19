964482b45778c70c862e43a94e8c35164fd2677a
namaah00 <kem.naam@gmail.com>
2026-01-19 05:09:11 +0000

    vit com

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,14 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import react from '@vitejs/plugin-react'; //plugin umożliwiający użycie reacta w vite
 
 export default defineConfig({
-  plugins: [react()],
-  base: './', // Ważne dla GitHub Pages!
+  plugins: [react()], //dodanie react plugin do vite
+  base: './', //wszystkie assety są ładowane relatywnie, więc strona działa również w repozytorium GitHub Pages
   server: {
     port: 5173,
     host: true
   },
   build: {
-    outDir: 'dist'
+    outDir: 'dist' //folder, do którego Vite wrzuci zbudowaną aplikację
   }
 });
